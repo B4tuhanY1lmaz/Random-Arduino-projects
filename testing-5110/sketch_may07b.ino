325aa51bef864c5e6061bbd488373bd5e979c454
Batuhan Yaşar Yılmaz <70820809+B4tuhanY1lmaz@users.noreply.github.com>
2022-05-08 00:35:02 +0300

    Add files via upload

---
+++ testing-5110/sketch_may07b.ino
@@ -0,0 +1,33 @@
+#include <LCD5110_Basic.h>
+
+int pushButton = 2;
+LCD5110 myGLCD(8,9,10,11,12);
+extern uint8_t SmallFont[];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(pushButton, INPUT);
+
+  
+  myGLCD.InitLCD();
+  myGLCD.setFont(SmallFont);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int buttonState = digitalRead(pushButton);
+  if (buttonState == HIGH) {
+    myGLCD.print("button pressed!", CENTER, 24);
+    delay(2000);
+    myGLCD.clrScr();
+  }
+ digitalWrite(LED_BUILTIN, HIGH);
+ Serial.write("Blink!");
+ delay(900);
+ digitalWrite(LED_BUILTIN, LOW);
+ delay(900);
+
+}
